50f3101e9bdf426477ea02bb7453f55c1d2400c3
anapaulaguimaraes <anaapgg@gmail.com>
2024-10-04 18:57:34 -0300

    :sparkles: feat: Definidas as rotas para listar países e salvar dados no banco.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CountriesController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [CountriesController::class, 'index']);
+Route::get('/save-countries', [CountriesController::class, 'fetchAndSaveCountries']);
